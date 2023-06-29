67deddd0ceff22b3e783998706c387f25635b2d9
Bloogefest <bloogefest@gmail.com>
2023-06-30 01:25:21 +0300

    Add test print function to bootloader.c

--- bootloader/bootloader.c
+++ bootloader/bootloader.c
@@ -1,3 +1,12 @@
+#include "bootloader.h"
+
+__attribute__((noinline)) __attribute__((regparm(3)))
+void real_printchar(char ch) {
+    __asm__ __volatile__("mov $0x0E, %%ah\n"
+                         "mov %0, %%al\n"
+                         "int $0x10" : : "a"(ch));
+}
+
 void bootloader_main() {
-    
+    real_printchar('k');
 }
