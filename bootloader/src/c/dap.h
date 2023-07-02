8453ba9d6be48266ac50aee4273bc6632e6935d1
Bloogefest <bloogefest@gmail.com>
2023-07-02 18:59:38 +0300

    Fix dap.h

--- bootloader/src/c/dap.h
+++ bootloader/src/c/dap.h
@@ -4,8 +4,8 @@
 
 extern volatile uint8_t x16_real_dap_size;
 extern volatile uint8_t x16_real_dap_reserved;
-extern volatile uint64_t x16_real_dap_number_of_sectors;
-extern volatile uint64_t x16_real_dap_buffer_offset;
-extern volatile uint64_t x16_real_dap_buffer_segment;
+extern volatile uint32_t x16_real_dap_number_of_sectors;
+extern volatile uint32_t x16_real_dap_buffer_offset;
+extern volatile uint32_t x16_real_dap_buffer_segment;
 extern volatile uint16_t x16_real_dap_lba_lower;
 extern volatile uint16_t x16_real_dap_lba_upper;
