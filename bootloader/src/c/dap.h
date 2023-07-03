2895e164d3bae05f8853968c047c69a9a25d24ba
Bloogefest <bloogefest@gmail.com>
2023-07-03 19:28:29 +0300

    Make dap.h compatible with last changes

--- bootloader/src/c/dap.h
+++ bootloader/src/c/dap.h
@@ -2,10 +2,10 @@
 
 #include <stdint.h>
 
-extern volatile uint8_t x16_real_dap_size;
-extern volatile uint8_t x16_real_dap_reserved;
-extern volatile uint32_t x16_real_dap_number_of_sectors;
-extern volatile uint32_t x16_real_dap_buffer_offset;
-extern volatile uint32_t x16_real_dap_buffer_segment;
-extern volatile uint16_t x16_real_dap_lba_lower;
-extern volatile uint16_t x16_real_dap_lba_upper;
+extern volatile uint8_t x16_realdap_size;
+extern volatile uint8_t dap_reserved;
+extern volatile uint32_t dap_number_of_sectors;
+extern volatile uint32_t dap_buffer_offset;
+extern volatile uint32_t dap_buffer_segment;
+extern volatile uint16_t dap_lba_lower;
+extern volatile uint16_t dap_lba_upper;
