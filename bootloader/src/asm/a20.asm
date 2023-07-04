ff64cf00ad92109bf9b94f68bad621833a5673b1
Bloogefest <bloogefest@gmail.com>
2023-07-04 03:04:04 +0300

    Rewrite a20.asm

--- bootloader/src/asm/a20.asm
+++ bootloader/src/asm/a20.asm
@@ -1,90 +1,113 @@
-x16_real_try_to_enable_the_a20_line:
-    call x16_real_check_the_a20_line
-    jne .already_enabled
-.try_enable_using_the_bios:
-    call x16_real_try_enable_the_a20_line_using_the_bios
+prepare_a20_line:
+    push si
+    mov si, PREPARING_A20_LINE
+    call println
+    mov si, CHECKING_A20_LINE
+    call println
+    call check_a20_line
     cmp ax, 0
-    je .enabled_using_the_bios
+    jz .already_enabled
+    mov si, A20_LINE_IS_NOT_ENABLED
+    call println
+    mov si, TRYING_TO_ENABLE_A20_LINE_USING_BIOS
+    call println
+    call enable_a20_line_using_bios
+    cmp ax, 0
+    jz .perhaps_enabled_using_bios
     cmp ax, 1
-    je .not_supported_using_the_bios
+    jz .not_supported_using_bios
     cmp ax, 2
-    je .could_not_get_status_using_the_bios
+    jz .could_not_get_status_using_bios
     cmp ax, 3
-    je .already_enabled_using_the_bios
-.could_not_enable_using_the_bios:
-    push si
-    mov si, x16_real_msg_the_a20_line_could_not_be_enabled_using_the_bios
+    jmp .perhaps_already_enabled_using_bios
+.could_not_enable_using_bios:
+    mov si, TRYING_TO_ENABLE_A20_LINE_USING_PS2_CONTROLLER
     call println
-    pop si
-.try_enable_using_the_keyboard_controller:
-    call x16_real_try_enable_the_a20_line_using_the_keyboard_controller
-    call x16_real_check_the_a20_line
-    jne .enabled_using_the_keyboard_controller
-.try_enable_using_the_fast_gate:
-    call x16_real_try_enable_the_a20_line_using_the_fast_gate
-    call x16_real_check_the_a20_line
-    jne .enabled_using_the_fast_gate
-.not_enabled_using_the_fast_gate:
-    push si
-    mov si, x16_real_msg_the_a20_line_could_not_be_enabled_using_the_fast_gate
+    call enable_a20_line_using_ps2_controller
+    call check_a20_line
+    cmp ax, 0
+    jz .enabled_using_ps2_controller
+    mov si, A20_LINE_IS_NOT_ENABLED_USING_PS2_CONTROLLER
     call println
-    pop si
-.not_enabled:
-    push si
-    mov si, x16_real_msg_failed_to_enable_the_a20_line
+    mov si, TRYING_TO_ENABLE_A20_LINE_USING_FAST_GATE
     call println
-    pop si
-    mov ax, 1
-    ret
+    call enable_a20_line_using_fast_gate
+    call check_a20_line
+    cmp ax, 0
+    jz .enabled_using_fast_gate
+    mov si, A20_LINE_COULD_NOT_BE_ENABLED_USING_FAST_GATE
+    call println
+    mov si, TRYING_TO_ENABLE_A20_LINE_USING_IO_PORT
+    call println
+    call enable_a20_line_using_io_port
+    call check_a20_line
+    cmp ax, 0
+    jz .enabled_using_io_port
+    mov si, A20_LINE_COULD_NOT_BE_ENABLED_USING_IO_PORT
+    call println
+    mov si, COULD_NOT_PREPARE_A20_LINE
+    call println
+    jmp .free
 .already_enabled:
-    push si
-    mov si, x16_real_msg_the_a20_line_already_enabled
+    mov si, A20_LINE_ALREADY_ENABLED
     call println
-    pop si
-    jmp .enabled
-.enabled_using_the_bios:
-    call x16_real_check_the_a20_line
-    je .try_enable_using_the_keyboard_controller
-    push si
-    mov si, x16_real_msg_the_a20_line_already_enabled
+    jmp .free
+.perhaps_enabled_using_bios:
+    mov si, PERHAPS_A20_LINE_IS_ENABLED_USING_BIOS
     call println
-    pop si
-    jmp .enabled
-.not_supported_using_the_bios:
-    push si
-    mov si, x16_real_msg_the_a20_line_enabling_is_not_supported_using_the_bios
+    mov si, CHECKING_A20_LINE
     call println
-    pop si
-    jmp .try_enable_using_the_keyboard_controller
-.could_not_get_status_using_the_bios:
-    push si
-    mov si, x16_real_msg_could_not_get_the_a20_line_status_using_the_bios
-    call println
-    pop si
-    jmp .try_enable_using_the_keyboard_controller
-.already_enabled_using_the_bios:
-    call x16_real_check_the_a20_line
-    je .try_enable_using_the_keyboard_controller
-    jmp .already_enabled
-.enabled_using_the_keyboard_controller:
-    push si
-    mov si, x16_real_msg_the_a20_line_is_enabled_using_the_keyboard_controller
-    call println
-    pop si
-    jmp .enabled
-.enabled_using_the_fast_gate:
-    call x16_real_check_the_a20_line
-    je .not_enabled_using_the_fast_gate
-    push si
-    mov si, x16_real_msg_the_a20_line_is_enabled_using_the_fast_gate
-    call println
-    pop si
-.enabled:
+    call check_a20_line
+    cmp ax, 0
+    jnz .not_enabled_using_bios
     mov ax, 0
+    mov si, A20_LINE_IS_INDEED_ENABLED_USING_BIOS
+    call println
+    jmp .free
+.not_enabled_using_bios:
+    mov si, A20_LINE_IS_NOT_ENABLED_USING_BIOS
+    call println
+    jmp .could_not_enable_using_bios
+.not_supported_using_bios:
+    mov si, A20_LINE_ENABLING_IS_NOT_SUPPORTED_USING_BIOS
+    call println
+    jmp .could_not_enable_using_bios
+.could_not_get_status_using_bios:
+    mov si, COULD_NOT_GET_A20_LINE_STATUS_USING_BIOS
+    call println
+    jmp .could_not_enable_using_bios
+.perhaps_already_enabled_using_bios:
+    mov si, PERHAPS_A20_LINE_IS_ALREADY_ENABLED_USING_BIOS
+    call println
+    mov si, CHECKING_A20_LINE
+    call println
+    call check_a20_line
+    cmp ax, 0
+    jnz .not_already_enabled_using_bios
+    mov ax, 0
+    mov si, A20_LINE_IS_INDEED_ALREADY_ENABLED_USING_BIOS
+    call println
+    jmp .free
+.not_already_enabled_using_bios:
+    mov si, A20_LINE_IS_NOT_ALREADY_ENABLED_USING_BIOS
+    call println
+    jmp .could_not_enable_using_bios
+.enabled_using_ps2_controller:
+    mov si, A20_LINE_IS_ENABLED_USING_PS2_CONTROLLER
+    call println
+    jmp .free
+.enabled_using_fast_gate:
+    mov si, A20_LINE_IS_ENABLED_USING_FAST_GATE
+    call println
+    jmp .free
+.enabled_using_io_port:
+    mov si, A20_LINE_IS_ENABLED_USING_IO_PORT
+    call println
+.free:
+    pop si
     ret
 
-x16_real_check_the_a20_line:
-.setup:
+check_a20_line:
     cli
     push ds
     push es
@@ -102,9 +125,8 @@ x16_real_check_the_a20_line:
     push dx
     mov byte [es:di], 0
     mov byte [ds:si], 0xFF
-.compare:
     cmp byte [es:di], 0xFF
-    je .disabled
+    jz .disabled
 .enabled:
     mov ax, 0
     jmp .free
@@ -122,14 +144,12 @@ x16_real_check_the_a20_line:
     sti
     ret
 
-x16_real_try_enable_the_a20_line_using_the_bios:
-.check_support:
+enable_a20_line_using_bios:
     mov ax, 0x2403
     int 0x15
     jb .not_supported
     cmp ah, 0
     jnz .not_supported
-.try_get_status:
     mov ax, 0x2402
     int 0x15
     jb .could_not_get_status
@@ -137,7 +157,6 @@ x16_real_try_enable_the_a20_line_using_the_bios:
     jnz .could_not_get_status
     cmp al, 1
     jz .already_enabled
-.try_enable:
     mov ax, 0x2401
     int 0x15
     jb .could_not_enable
@@ -159,14 +178,7 @@ x16_real_try_enable_the_a20_line_using_the_bios:
     mov ax, 4
     ret
 
-x16_real_disable_the_a20_line_using_the_bios:
-    push ax
-    mov ax, 0x2400
-    int 0x15
-    pop ax
-    ret
-
-x16_real_try_enable_the_a20_line_using_the_keyboard_controller:
+enable_a20_line_using_ps2_controller:
     cli
     call .wait
     mov al, 0xAD
@@ -201,25 +213,44 @@ x16_real_try_enable_the_a20_line_using_the_keyboard_controller:
     jz .co_wait
     ret
 
-x16_real_try_enable_the_a20_line_using_the_fast_gate:
+enable_a20_line_using_fast_gate:
     in al, 0x92
     test al, 2
     jnz .free
     or al, 2
     and al, 0xFE
     out 0x92, al
-    jmp .free
 .free:
     ret
 
-x16_real_msg_trying_to_enable_the_a20_line: db 'Trying to enable the A20 line...', 0
-x16_real_msg_the_a20_line_already_enabled: db 'The A20 line already enabled', 0
-x16_real_msg_the_a20_line_is_enabled_using_the_bios: db 'The A20 line is enabled using the BIOS', 0
-x16_real_msg_the_a20_line_enabling_is_not_supported_using_the_bios: db 'The A20 line enabling is not supported using the BIOS', 0
-x16_real_msg_could_not_get_the_a20_line_status_using_the_bios: db 'Could not get the A20 line status using the BIOS', 0
-x16_real_msg_the_a20_line_could_not_be_enabled_using_the_bios: db 'The A20 line could not be enabled using the BIOS', 0
-x16_real_msg_the_a20_line_is_enabled_using_the_keyboard_controller: db 'The A20 line is enabled using the keyboard controller', 0
-x16_real_msg_the_a20_line_is_enabled_using_the_fast_gate: db 'The A20 line is enabled using the fast gate', 0
-x16_real_msg_the_a20_line_could_not_be_enabled_using_the_fast_gate: db 'The A20 line could not be enabled using the fast gate', 0
-x16_real_msg_failed_to_enable_the_a20_line: db 'Failed to enable the A20 line', 0
-x16_real_msg_correct_behavior_with_the_disabled_a20_line_is_not_guaranteed: db 'Correct behavior with the disabled A20 line is not guaranteed', 0
+enable_a20_line_using_io_port:
+    in al, 0xEE
+    ret
+
+disable_a20_line_using_io_port:
+    out 0xEE, al
+    ret
+
+PREPARING_A20_LINE: db 'Preparing the A20 line...', 0
+CHECKING_A20_LINE: db 'Checking the A20 line...', 0
+A20_LINE_ALREADY_ENABLED: db 'The A20 line already enabled', 0
+A20_LINE_IS_NOT_ENABLED: db 'The A20 line is not enabled', 0
+TRYING_TO_ENABLE_A20_LINE_USING_BIOS: db 'Trying to enable the A20 line using the BIOS...', 0
+PERHAPS_A20_LINE_IS_ENABLED_USING_BIOS: db 'Perhaps the A20 line is enabled using the BIOS', 0
+A20_LINE_IS_INDEED_ENABLED_USING_BIOS: db 'The A20 line is indeed enabled using the BIOS', 0
+A20_LINE_IS_NOT_ENABLED_USING_BIOS: db 'The A20 line is not enabled using the BIOS', 0
+A20_LINE_ENABLING_IS_NOT_SUPPORTED_USING_BIOS: db 'The A20 line enabling is not supported using the BIOS', 0
+COULD_NOT_GET_A20_LINE_STATUS_USING_BIOS: db 'Could not get the A20 line status using the BIOS', 0
+PERHAPS_A20_LINE_IS_ALREADY_ENABLED_USING_BIOS: db 'Perhaps the A20 line is already enabled using the BIOS', 0
+A20_LINE_IS_INDEED_ALREADY_ENABLED_USING_BIOS: db 'The A20 line is indeed already enabled using the BIOS', 0
+A20_LINE_IS_NOT_ALREADY_ENABLED_USING_BIOS: db 'The A20 line is not already enabled using the BIOS', 0
+TRYING_TO_ENABLE_A20_LINE_USING_PS2_CONTROLLER: db 'Trying to enable the A20 line using the PS/2 controller...', 0
+A20_LINE_IS_ENABLED_USING_PS2_CONTROLLER: db 'The A20 line is enabled using the PS/2 controller', 0
+A20_LINE_IS_NOT_ENABLED_USING_PS2_CONTROLLER: db 'The A20 line is not enabled using the PS/2 controller', 0
+TRYING_TO_ENABLE_A20_LINE_USING_FAST_GATE: db 'Trying to enable the A20 line using the fast gate...', 0
+A20_LINE_IS_ENABLED_USING_FAST_GATE: db 'The A20 line is enabled using the fast gate', 0
+A20_LINE_COULD_NOT_BE_ENABLED_USING_FAST_GATE: db 'The A20 line could not be enabled using the fast gate', 0
+TRYING_TO_ENABLE_A20_LINE_USING_IO_PORT: db 'Trying to enable the A20 line using the 0xEE IO port...', 0
+A20_LINE_IS_ENABLED_USING_IO_PORT: db 'The A20 line is enabled using the 0xEE IO port', 0
+A20_LINE_COULD_NOT_BE_ENABLED_USING_IO_PORT: db 'The A20 line could not be using the 0xEE IO port', 0
+COULD_NOT_PREPARE_A20_LINE: db 'Could not prepare the A20 line', 0
