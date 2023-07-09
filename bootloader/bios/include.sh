d3c8215926fdadf7e839494ed9cffa4c3a7cd828
Bloogefest <bloogefest@gmail.com>
2023-07-09 20:59:52 +0300

    Remove include.sh

--- bootloader/bios/include.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-BOOTLOADER_SIZE=$(wc -c < build/i686/asm/bootloader.bin)
-if [[ $((BOOTLOADER_SIZE % 512)) == 0 ]]; then
-  BOOTLOADER_SIZE-=512
-fi
-INCLUDE_NUMBER_OF_LATE_SECTORS=$((BOOTLOADER_SIZE / 512))
-printf "%%define INCLUDE_NUMBER_OF_LATE_SECTORS %s" $INCLUDE_NUMBER_OF_LATE_SECTORS > build/i686/asm/include.asm
