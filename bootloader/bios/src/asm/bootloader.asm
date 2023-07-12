174bc2bd16d6b73b98687979a55281600a0504d4
Bloogefest <bloogefest@gmail.com>
2023-07-13 02:58:05 +0300

    [BIOS Bootloader] Make `bootloader.asm` better

--- bootloader/bios/src/asm/bootloader.asm
+++ bootloader/bios/src/asm/bootloader.asm
@@ -23,78 +23,87 @@ times 90 db 0
 jmp 0x0:entry
 
 entry:
-    xor ax, ax
-    mov ds, ax
+    mov [disk], dl ; Сохранение загрузочной информации.
+
+    xor ax, ax ; Подготовка общих регистров.
+    xor bx, bx
+    xor cx, cx
+    xor dx, dx
+    xor si, si
+    xor di, di
+
+    mov bp, ax ; Подготовка стековых регистров.
+    mov sp, entry
+
+    mov ds, ax ; Подготовка сегментных регистров.
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    mov sp, entry
-    mov [disk], dl
-    cld
-    mov ax, INCLUDE_NUMBER_OF_LATE_SECTORS
+
+    mov si, READING_LATE_SECTORS ; Чтение поздних секторов с диска в память.
+    call writeln
+
+    mov ax, NUMBER_OF_LATE_SECTORS
     mov bx, entrance
     xor cx, cx
     mov edx, 1
-    mov si, LATE_SECTORS_READING
-    call writeln
     call read_sectors
-    jc .error
-    mov si, JUMPING_TO_LATE_SECTORS
+    jc .halt
+
+    mov si, TRANSFER_OF_CONTROL_TO_LATE_SECTORS ; Передача управления поздним секторам.
     call writeln
+
     jmp entrance
-.error:
-    mov si, LATE_SECTORS_READING_ERROR
-    call writeln
 .halt:
     hlt
     jmp .halt
 
 write:
     push ax
-    push si
     mov ah, 0xE
 .loop:
     lodsb
     cmp al, 0
-    jz .free
+    je .free
     int 0x10
     jmp .loop
 .free:
-    pop si
     pop ax
     ret
 
 writeln:
     call write
-    push si
-    mov si, LN_SEPARATOR
+    mov si, LINE_SEPARATOR
     call write
-    pop si
     ret
 
 read_sectors:
     cmp ax, 127
     jbe .reading
+
     push ax
     mov ax, 127
     call read_sectors
-    jc .error
     pop ax
+    jc .free
+
     sub ax, 127
     add cx, 127 * 512 / 16
     add edx, 127
+
     jmp read_sectors
 .reading:
     mov [dap_number_of_sectors], ax
     mov [dap_buffer_offset], bx
     mov [dap_buffer_segment], cx
     mov [dap_lba_lower], edx
+
+    mov ah, 0x42
     mov dl, [disk]
     mov si, dap
-    mov ah, 0x42
     int 0x13
-.error:
+.free:
     ret
 
 disk: db 0x80
@@ -108,10 +117,9 @@ dap_buffer_segment: dw 0
 dap_lba_lower: dd 0
 dap_lba_upper: dd 0
 
-LN_SEPARATOR: db 13, 10, 0
-LATE_SECTORS_READING: db 'Reading the late sectors...', 0
-LATE_SECTORS_READING_ERROR: db 'Unable to read the late sectors', 0
-JUMPING_TO_LATE_SECTORS: db 'Jumping to the late sectors...', 0
+LINE_SEPARATOR: db 13, 10, 0
+READING_LATE_SECTORS: db 'Reading the late sectors from disk to memory...', 0
+TRANSFER_OF_CONTROL_TO_LATE_SECTORS: db 'Transfer of control to the late sectors...', 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
