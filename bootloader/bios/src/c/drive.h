dc423b2531d36c0c48800a71e4a7c2eed5abd36f
Bloogefest <bloogefest@gmail.com>
2023-07-12 21:44:06 +0300

    [BIOS Bootloader] Make `drive_parameters_s` better

--- bootloader/bios/src/c/drive.h
+++ bootloader/bios/src/c/drive.h
@@ -3,13 +3,13 @@
 #include <stdint.h>
 
 typedef struct drive_parameters_s {
-    uint16_t size;
-    uint16_t flags;
-    uint32_t number_of_cylinders;
-    uint32_t number_of_heads;
-    uint32_t number_of_sectors_per_track;
-    uint64_t number_of_sectors;
-    uint16_t number_of_bytes_per_sector;
+    volatile uint16_t size;
+    volatile uint16_t flags;
+    volatile uint32_t number_of_cylinders;
+    volatile uint32_t number_of_heads;
+    volatile uint32_t number_of_sectors_per_track;
+    volatile uint64_t number_of_sectors;
+    volatile uint16_t number_of_bytes_per_sector;
 } __attribute__((__packed__)) drive_parameters_t;
 
 extern volatile drive_parameters_t drive_parameters;
