17f3cca849c1576e1a5bdafcb163ab4e90017c4c
Bloogefest <bloogefest@gmail.com>
2023-07-15 18:45:03 +0300

    [BIOS Bootloader] Make `bootloader.c` better

--- bootloader/bios/src/c/bootloader.c
+++ bootloader/bios/src/c/bootloader.c
@@ -3,6 +3,8 @@
 __asm__(".code16gcc");
 
 void bootloader_main() {
-    println_s("daroy");
+    println_s("=== UNREAL MODE ===");
+    switch_to_protected_mode();
+    protected_println_s("=== PROTECTED MODE ===");
     //enter_protected_mode();
 }
