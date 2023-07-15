1c1f12368c945a79ac0896320716dd0d6dda602c
Bloogefest <bloogefest@gmail.com>
2023-07-15 18:45:28 +0300

    [BIOS Bootloader] Make `gdt.asm` better

--- bootloader/bios/src/asm/gdt.asm
+++ bootloader/bios/src/asm/gdt.asm
@@ -1,9 +1,31 @@
+extern writeln
+
+global prepare_gdt
+global load_gdt
 global gdt
 global gdt_base
 global gdt_end
 
 bits 16
 section .gdt
+
+prepare_gdt:
+    push si
+    mov si, PREPARING_GDT
+    call writeln
+    pop si
+
+    call load_gdt
+    ret
+
+load_gdt:
+    push si
+    mov si, LOADING_GDT
+    call writeln
+    pop si
+
+    lgdt [gdt]
+    ret
 
 align 8, db 0
 
@@ -42,3 +64,6 @@ gdt_base:
 gdt_end:
 
 align 4, db 0
+
+PREPARING_GDT: db 'Preparing the GDT...', 0
+LOADING_GDT: db 'Loading the GDT...', 0
