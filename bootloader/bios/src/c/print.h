4f144f9013e067a512bc922e617083dcfefdd607
Bloogefest <bloogefest@gmail.com>
2023-07-15 18:46:08 +0300

    [BIOS Bootloader] Make `print.h` better

--- bootloader/bios/src/c/print.h
+++ bootloader/bios/src/c/print.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "stdint.h"
+
 #define LN_SEPARATOR "\r\n"
 
 void print_c(const char c);
@@ -11,3 +13,13 @@ void println();
 void println_c(const char c);
 
 void println_s(const char *s);
+
+void protected_print_c(const char c);
+
+void protected_print_s(const char *s);
+
+void protected_println();
+
+void protected_println_c(const char c);
+
+void protected_println_s(const char *s);
