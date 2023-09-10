3e410a3fd54834e8eec2475a5e20f70e8adcb840
Bloogefest <bloogefest@gmail.com>
2023-09-10 14:24:19 +0300

    Добавим адаптер для загрузочной информации

--- bootloader/bios/src/intel64/c/injector.h
+++ bootloader/bios/src/intel64/c/injector.h
@@ -16,3 +16,6 @@ void println_adapter(const char *string);
 
 void read_sectors_adapter(uint16_t number_of_sectors, uint16_t buffer_offset, uint16_t buffer_segment,
                           uint16_t lba_lower);
+
+extern uint8_t disk;
+extern uint8_t number_of_sectors;
