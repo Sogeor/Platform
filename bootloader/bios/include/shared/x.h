5c69fb413e46347b4c2ffd00f9ff8e5e1ed04f08
Bloogefest <bloogefest@gmail.com>
2023-11-03 15:39:30 +0300

    Удаление лишних файлов

--- bootloader/bios/include/shared/x.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void x_print(const char *string);
-
-void x_println(const char *string);
-
-void x_read_sectors(uint16_t number_of_sectors, uint16_t buffer_offset, uint16_t buffer_segment, uint16_t lba_lower);
