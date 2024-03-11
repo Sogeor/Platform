f5942e0a55feefcb8419c0b18fc0af269f3dee78
Bloogefest <bloogefest@gmail.com>
2024-03-11 23:54:07 +0300

    Сохранение изменений перед переходом с NASM на GAS

---
+++ x86/boot/boot.h
@@ -0,0 +1,9 @@
+#ifndef PLATFORM_BOOT_BOOT_H
+#define PLATFORM_BOOT_BOOT_H
+
+#include <stdint.h>
+
+__attribute__((noreturn))
+void main();
+
+#endif // PLATFORM_BOOT_BOOT_H
--- x86/boot/main.c
+++ x86/boot/main.c
@@ -2,17 +2,10 @@
 
 __asm__(".code16gcc");
 
-#include "interrupt.h"
-
 extern void header_die();
 
 __attribute__((noreturn))
-void main() {
-    interrupt_dump();
-//    interrupt_istate.ah = 0xE;
-//    interrupt_istate.al = 13;
-//    interrupt(0x10);
-//    interrupt_istate.al = 10;
-//    interrupt(0x10);
-    header_die();
+void main(void)
+{
+
 }
